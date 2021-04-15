817aa37c45ddccd14b490e73fe9fa5e9b249ca35
Hollie Baker <hb581@linux1.bath.ac.uk>
2021-04-15 17:08:03 +0100

    initial commit

---
+++ linked_list.c
@@ -0,0 +1,33 @@
+/*==============================================================================
+  Singly Linked List
+==============================================================================*/
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <stdbool.h>
+#include "linked_list.h"
+
+// global variable to store the list
+Node *linked_list = NULL;
+
+// push string onto front of list
+void push_item(const char *value)
+{
+}
+
+const char *get_item(int i)
+{
+    return NULL;
+}
+
+bool remove_item(int i)
+{
+    return false;
+}
+
+void print_list()
+{
+    printf("TODO\n");
+}
+
---
+++ linked_list.h
@@ -0,0 +1,28 @@
+/*==============================================================================
+  Singly Linked List
+==============================================================================*/
+
+#ifndef LINKED_LIST_H
+#define LINKED_LIST_H
+
+#include <stdbool.h>
+
+typedef struct _Node {
+    char *value;
+    struct _Node *next;
+} Node;
+
+// push string onto front of list
+void push_item(const char *value);
+
+// retrieve the i-th element, null if there is none
+const char *get_item(int i);
+
+// remove element at index i. returns true if successful, false otherwise
+bool remove_item(int i);
+
+// print list to standard out
+void print_list();
+
+#endif
+
